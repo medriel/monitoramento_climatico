6606e734274f7b6c2f4ec1fae013bf273d8f22e2
adriel.souza397@gmail.com <adriel.souza397@gmail.com>
2021-09-19 20:24:56 -0300

    Ajustando código do arduino para leitura no supervisório

--- Arduino/monitoramento_climatico/monitoramento_climatico.ino
+++ Arduino/monitoramento_climatico/monitoramento_climatico.ino
@@ -23,7 +23,7 @@ const int pinoSensorChuva = A3;
 const int pinoSensorHigrometro = A4;
 // --------------------
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(2000000);
   dht.begin(); //DHT 11
   pinMode(pinoLDR, INPUT); // LDR
    pinMode(MQ_analog, INPUT); // MQ-2
@@ -33,74 +33,74 @@ void setup() {
 }
 
 void sensor_DHT11(){
-  Serial.print("Umidade: ");
-  Serial.print(dht.readHumidity()); //IMPRIME NA SERIAL O VALOR DE UMIDADE MEDIDO
-  Serial.print("%");
-  Serial.print(" / Temperatura: ");
-  Serial.print(dht.readTemperature(), 0); //IMPRIME NA SERIAL O VALOR DE UMIDADE MEDIDO E REMOVE A PARTE DECIMAL
-  Serial.println("*C");
+//  Serial.print("Umidade: ");
+  Serial.println((int)dht.readHumidity()); //IMPRIME NA SERIAL O VALOR DE UMIDADE MEDIDO
+//  Serial.print("%");
+//  Serial.print(" / Temperatura: ");
+  Serial.println(dht.readTemperature(), 0); //IMPRIME NA SERIAL O VALOR DE UMIDADE MEDIDO E REMOVE A PARTE DECIMAL
+//  Serial.println("*C");
 }
 
 void sensor_LDR(){
   int valor_analogico = analogRead(pinoLDR);
   Serial.println(valor_analogico);
-  if(valor_analogico > 350){
-    Serial.println("Noite");
-  }  
-  else{ 
-    Serial.println("Dia");
-  }  
+//  if(valor_analogico > 350){
+//    Serial.println("Noite");
+//  }  
+//  else{ 
+//    Serial.println("Dia");
+//  }  
 }
 
 void sensor_MQ_2(){
   int valor_analogico = analogRead(MQ_analog);
   Serial.println(valor_analogico);
-  if(valor_analogico >100){
-    Serial.println("Indicio de incêndio");
-  }else{
-    Serial.println("Qualidade do ar normal");
-  }
+//  if(valor_analogico >100){
+//    Serial.println("Indicio de incêndio");
+//  }else{
+//    Serial.println("Qualidade do ar normal");
+//  }
 }
 
 void sensor_de_chuva(){
   int valor_analogico = analogRead(pinoSensorChuva);
   Serial.println(valor_analogico);
-  if(valor_analogico<900 && valor_analogico>300){
-    Serial.println("Chuva leve");
-  }else if(valor_analogico<300){
-    Serial.println("Chuva intensa");
-  }else{
-    Serial.println("Não chove no momento");
-  }
+//  if(valor_analogico<900 && valor_analogico>300){
+//    Serial.println("Chuva leve");
+//  }else if(valor_analogico<300){
+//    Serial.println("Chuva intensa");
+//  }else{
+//    Serial.println("Não chove no momento");
+//  }
 }
 
 void sensor_higrometro(){
   int valor_analogico = analogRead(pinoSensorHigrometro);
 
   Serial.println( valor_analogico);
-  
-  if(valor_analogico > 0 && valor_analogico < 400){
-    Serial.println(" Status: Solo umido");
-  }
-  if (valor_analogico > 400 && valor_analogico < 800){
-    Serial.println(" Status: Umidade moderada");
-  }
-  if (valor_analogico > 800 && valor_analogico < 1024){
-    Serial.println(" Status: Solo seco");
-  }
+//  
+//  if(valor_analogico > 0 && valor_analogico < 400){
+//    Serial.println(" Status: Solo umido");
+//  }
+//  if (valor_analogico > 400 && valor_analogico < 800){
+//    Serial.println(" Status: Umidade moderada");
+//  }
+//  if (valor_analogico > 800 && valor_analogico < 1024){
+//    Serial.println(" Status: Solo seco");
+//  }
 }
 
 void loop() {
-    Serial.println("----------- Sensor DHT 11 -------------");
+//    Serial.println("----------- Sensor DHT 11 -------------");
     sensor_DHT11();
-    Serial.println("------------- Sensor LDR --------------");
+//    Serial.println("------------- Sensor LDR --------------");
     sensor_LDR();
-    Serial.println("------------ Sensor MQ-2 --------------");
+//    Serial.println("------------ Sensor MQ-2 --------------");
     sensor_MQ_2();
-    Serial.println("---------- Sensor de chuva ------------");
+//    Serial.println("---------- Sensor de chuva ------------");
     sensor_de_chuva();
-    Serial.println("---- Sensor de Humidade do solo ------");
+//    Serial.println("---- Sensor de Humidade do solo ------");
     sensor_higrometro();
-    Serial.println("\n\n\n\n");
+//    Serial.println("\n\n\n\n");
     delay(2000);
 }
